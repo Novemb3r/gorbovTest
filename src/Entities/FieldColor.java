2ec9b38fb738239a900b8cb2ad0379d3ecfa6c23
Novemb3r <igkutyrev@gmail.com>
2018-09-27 21:37:19 +0700

    логика первой стадии3

--- src/Entities/FieldColor.java
+++ src/Entities/FieldColor.java
@@ -1,6 +1,7 @@
 package Entities;
 
 public class FieldColor {
+
     public static int RED = 1;
 
     public static int BLACK = 0;
--- src/Models/Stages/StageAbstract.java
+++ src/Models/Stages/StageAbstract.java
@@ -4,12 +4,6 @@ public abstract class StageAbstract {
 
     protected int[] currentNumber;
 
-    /**
-     * 0 - черное
-     * 1 - красное
-     * <p>
-     * Плохое решение, любые лучшие решения принимаются
-     */
     protected int currentColor;
 
     protected abstract void switchCurrentColor();
